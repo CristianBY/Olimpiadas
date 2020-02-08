93096e4e5008c6138872b74106227db45fd58c06
lbullon <lbullon86@gmail.com>
2020-02-08 13:20:44 +0100

    mejorando clase Usuario y subiendo tablas de la db

--- minijuegos/Model/Usuario.sql
+++ minijuegos/Model/Usuario.sql
@@ -1,7 +1,12 @@
-create table users (
-    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    firstName varchar(100),
-    surnames varchar (500),
-    course varchar(300)
+    create table users (
+        id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        firstName varchar(100),
+        surnames varchar (500),
+        course varchar(300)
 
-);
+    );
+
+    create table puntuaciones (
+        id int(11) NOT NULL PRIMARY KEY,
+        puntuacion int(20)
+    )

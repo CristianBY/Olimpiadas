3105727ab8dee1e963118b09cc67a04d154725af
alumnado <alumnado@A114-PC02.iespoligonosur.org>
2020-02-11 10:27:32 +0100

    contol login

---
+++ minijuegos/Controller/loginController.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    require("../View/vistaIndex.html");
+    require("../Model/Usuario.php");
+    $user = new Usuario($_POST['nameGroup'],$_POST['course']);
+    $validation = $user->verificarUsuario(); // Verificación de usuario en la base de datos
+
+    if (isset($_POST['enviar'])){
+        if ($validation){
+            $_SESSION['userLogIn'] = $user;
+            header('Location: ../index.php');
+        } else {
+            header('Location: ../Controller/selectionController.php');
+        }    
+    }
+    if (isset($_POST['verPuntuacion'])){
+        header('Location: Controller/puntuacionController.php');
+    }    
+?>

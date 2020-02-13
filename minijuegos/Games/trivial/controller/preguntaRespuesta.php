6cba06f810cd1a709479cc4628e5dff8bda7cd7d
lbullon <lbullon86@gmail.com>
2020-02-13 23:29:43 +0100

    13/02

--- minijuegos/Games/trivial/controller/preguntaRespuesta.php
+++ minijuegos/Games/trivial/controller/preguntaRespuesta.php
@@ -5,13 +5,12 @@ include("functions.php" );
 $respuestas = getAnswers($_POST['id']);//array
 $pregunta = getQuestion($_POST['id']);//String
 
-
 $enviado = array();
 array_push($enviado,$respuestas);
 array_push($enviado,$pregunta);
 
 echo json_encode($enviado);
-echo "prueba";
+
 
 
 
--- minijuegos/Games/trivial/view/scripts/script.js
+++ minijuegos/Games/trivial/view/scripts/script.js
@@ -1,40 +1,59 @@
 $(document).ready(function() {
-    
+    if (sessionStorage['trivialPreguntas'] == null) {
+        sessionStorage.setItem('trivialPreguntas', 0);
+        alert("Existe" + sessionStorage['trivialPreguntas'])
+
+    } else {
+        sessionStorage['trivialPreguntas']++;
+
+        alert("No existe" + sessionStorage['trivialPreguntas'])
+
+    }
     //Obtenemos un número aleatorio de 1 a 10
+
     var aleatorio = getRandomArbitrary(10, 1);
-    
+
 
     //Creamos petición de preguntas y posibles respuestas a servidor
     $.ajax({
-        url: "../../controller/preguntaRespuesta.php",
+        url: "../controller/preguntaRespuesta.php",
         type: 'post',
         datatype: 'json',
         data: { id: aleatorio },
         success: function(response) {
-            
+
             var json = JSON.parse(response);
             $("#pregunta").text(json[1]);
             $("#A").text(json[0].respuesta1);
             $("#B").text(json[0].respuesta2);
             $("#C").text(json[0].respuesta3);
             $("#D").text(json[0].respuesta4);
+
         }
     })
 
 
     //Creamos evento al pulsar botón
     $(".btn").click(function() {
-        $(this).css("background-color", "red");
+        $(this).css("background-color", "yellow");
+        var boton = $(this);
+
+        var eleccion = setInterval(function() { boton.toggle() }, 200);
+
         var respuesta = $(this).attr("id");
         //Hacemos petición al servidor para comprobar respuestas.
         $.ajax({
-            url: "../Games/trivial/controller/correcta.php",
+            url: "../controller/correcta.php",
             type: 'post',
             datatype: 'json',
             data: { id: aleatorio },
             success: function(response) {
                 var correcta = response.replace(/['"]+/g, '');
-                isCorrect(respuesta, correcta);
+                setTimeout(function() {
+                    isCorrect(respuesta, correcta);
+                    clearInterval(eleccion);
+
+                }, 3000);
 
             }
         })
@@ -44,10 +63,24 @@ $(document).ready(function() {
 
 //Función que comprueba que sea correcto;
 function isCorrect(respuesta, correcta) {
+    respuestaSelector = "#" + respuesta;
+
     if (respuesta == correcta) {
-        alert("ACERTADOddd");
-        window.location.href = "index.html";
-    } else(alert("ERRROOOOR ,INUTIL"))
+
+        $(respuestaSelector).toggle();
+        $(respuestaSelector).css("background-color", "green")
+        setTimeout(function() {
+            window.location.href = "index.html";
+        }, 2000);
+
+
+    } else {
+        $(respuestaSelector).toggle();
+        $(respuestaSelector).css("background-color", "red")
+        setTimeout(function() {
+            window.location.href = "index.html";
+        }, 2000);
+    }
 }
 
 function getRandomArbitrary(min, max) {

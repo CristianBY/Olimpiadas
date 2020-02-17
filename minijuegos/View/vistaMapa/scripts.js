caf1e50461ba67208b8af76a6c20b5a1f31d7b02
CristianBY <cristianyelmo@gmail.com>
2020-02-18 00:57:07 +0100

    enlace de puntuación correcto

--- minijuegos/View/vistaMapa/scripts.js
+++ minijuegos/View/vistaMapa/scripts.js
@@ -53,7 +53,7 @@ function movimientoMario() {
             $("#mario").animate({ left: "85%", }, 3100);
             sessionStorage.setItem('mijuego', 0);
             setTimeout(() => {
-                window.location.replace("http://localhost/Olimpiadas/minijuegos/View/puntuaction.html");
+                window.location.replace("puntuaction.html");
             }, 2000);
 
         default:

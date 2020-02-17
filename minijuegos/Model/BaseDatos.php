5952119b647fc6f75f95235efd1aa9a82d46ed48
CristianBY <cristianyelmo@gmail.com>
2020-02-17 21:30:05 +0100

    AWS

--- minijuegos/Model/BaseDatos.php
+++ minijuegos/Model/BaseDatos.php
@@ -2,7 +2,7 @@
 abstract class BaseDatos
 {
 
-    private static $server = '34.205.30.97';
+    private static $server = 'localhost';
     private static $dataBase = 'olimpiadas';
     private static $user = 'olimpiadas';
     private static $pass = 'abc123.';

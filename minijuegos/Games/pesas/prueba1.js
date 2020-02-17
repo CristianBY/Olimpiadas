6a9d3682824a1817e1b57f3f9b557908f055e19e
JoseLQB <josequb88@gmail.com>
2020-02-17 10:14:00 +0100

    solucionado problema con pesas

--- minijuegos/Games/pesas/prueba1.js
+++ minijuegos/Games/pesas/prueba1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
     pressStart();
     $(".button").click(function(){
@@ -12,54 +11,41 @@ $(document).ready(function(){
 //Levantamiento de pesas pulsando <- y ->
 function levanta(){
     $(".button").hide();//Esconde el botón start
-=======
-$(document).ready(function() {
-        levanta();
-        //Movimiento de escenario
-        $('body').animate({
-            'background-position-x': '8080%'
-        }, 50000, 'linear');
-
-    })
-    //Levantamiento de pesas pulsando <- y ->
-function levanta() {
->>>>>>> 98b691e84f1dcd0cf36ed1317e024ef61fe2cb45
     var j = 600;
     var fetch = true;
     var fallo = 0;
 
-    var crono = setInterval(demo(), 20);
-
-    $(document).bind("keyup", function(event) {
-
-        var key = (event.keyCode ? event.keyCode : event.which);
-        if (key == "37") {
+    var crono=setInterval(demo(), 20);
+    
+    $(document).bind("keyup", function(event) { 
+        
+        var key = (event.keyCode ? event.keyCode : event.which); 
+        if (key =="37"){
             fetch = false;
         }
-        if (key == "39" && fetch == false) {
-            if (j > 0) {
-                j = j - 20;
+        if (key == "39" && fetch == false){
+            if(j>0){
+                j=j-20;
                 fetch = true;
             }
-        } else {
-            fallo = fallo + 1;
-            if (fallo == 2) {
+        }else{
+            fallo = fallo +1;
+            if(fallo == 2){
                 j = j + 20;
                 fallo = 0;
             }
         }
         ///
         console.log(fallo);
-        $("#pesas").css({ top: j + "px" });
+        $("#pesas").css({ top: j + "px" });  
         //Para el crono cuando la pesa llega arriba y guarda el tiempo tardado 
-        if (j == 100) {
+        if(j == 100){
             clearInterval(crono);
             $(document).off("keyup");
             var resultado = $("#output").text();
-            if (resultado == "00:00") {
+            if(resultado == "00:00"){
                 $(document).off("keyup");
                 clearInterval(crono);
-<<<<<<< HEAD
                 $("#output").text("No has podido levantar la pesa en el tiempo establecido, otra vez será. ")
                 setTimeout(function (){window.location.replace("../vistaMapa/selectGame.html")}, 3000);
                 
@@ -68,36 +54,24 @@ function levanta() {
             $("#output").text("Tu puntuación ha sido " +  Math.floor(puntuacion));
             sessionStorage.miPuntuaction = parseInt(sessionStorage.miPuntuaction) + puntuacion;
             setTimeout(function (){window.location.replace("../vistaMapa/selectGame.html")}, 3000);
-=======
-                $("#output").text("No has podido levantar la pesa en el tiempo establecido, otra vez será. ");
-                setTimeout(function() { window.location.replace("../../View/vistaMapa/selectGame.html") }, 4000);
-            }
-            var puntuacion = parseInt(calculaPuntuacion(resultado))
-            $("#output").text("Tu puntuación ha sido " + Math.floor(puntuacion));
-
->>>>>>> 98b691e84f1dcd0cf36ed1317e024ef61fe2cb45
 
         }
-        if (j > 630) {
+        if(j>630){
             $(document).off("keyup");
             clearInterval(crono);
-<<<<<<< HEAD
             $("#output").text("No puedes levantar la pesa, otra vez será. ");
             setTimeout(function (){window.location.replace("../vistaMapa/selectGame.html")}, 3000); 
-=======
-            $("#output").text("No puedes levantar la pesa, otra vez será. ")
->>>>>>> 98b691e84f1dcd0cf36ed1317e024ef61fe2cb45
         }
-        if (j == 550) {
+        if(j==550){
             //Estado 1
         }
-        if (j == 500) {
+        if(j==500){
             //Estado 2
         }
-        if (j == 450) {
+        if(j == 450){
             //Estado 3
         }
-    });
+    }); 
 }
 
 //Crea el botón start que da inicio a la carrera
@@ -134,20 +108,20 @@ function formatSeconds(secs) {
     var h = Math.floor(secs / 3600);
     var m = Math.floor(secs / 60) - (h * 60);
     var s = Math.floor(secs - h * 3600 - m * 60);
-    if (s < 10 && s > 0) {
+    if(s<10 && s>0){
         var temp = pad(m) + ":0" + pad(s);
-        var puntuacion = pad(m) + "0" + pad(s);
+        var puntuacion = pad(m) + "0" +pad(s);
         console.log(puntuacion);
-    } else {
+    }else{
         var temp = pad(m) + ":" + pad(s);
-        var puntuacion = pad(m) + "" + pad(s);
+        var puntuacion = pad(m) + "" +pad(s);
         console.log(puntuacion);
     }
     return temp;
 }
 
 //La puntuación se calcula a partir del tiempo
-function calculaPuntuacion(tiempo) {
+function calculaPuntuacion(tiempo){
     var puntua = tiempo.split(":");
     var devuelvePuntos = "";
     puntua.forEach(i => {
@@ -156,3 +130,5 @@ function calculaPuntuacion(tiempo) {
 
     return devuelvePuntos;
 }
+
+

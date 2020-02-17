5c4d4303fe8be2ee6f61c180eff0508eb147f2e9
Zoraida Rios <zoraidariosajdie@gmail.com>
2020-02-17 18:29:46 +0100

    Vista acabada en bootStrap

--- minijuegos/Games/quien/script/javas.js
+++ minijuegos/Games/quien/script/javas.js
@@ -6,32 +6,33 @@ $(document).ready(function(){
 		var src;
 		var pista;
 		var cont = 0;
+		/*Recorre lista json*/
 		for(var i = 0; i<obj.length ; i++){
+			/*Para numero aleatorio del 0 al 5*/
 			aleatorio= Math.floor(Math.random() * 5);
+			/*Para poner la pregunta*/
 			$("h3").text(obj[aleatorio].pregunta);
 			src = obj[aleatorio].respuesta;
 			pista = obj[aleatorio].pistas;
 		}
+		/*Hacen los flip */
 		$("#card").flip({axis: 'x'}); 
 		$("#card1").flip({ axis: 'x' }); 
 		$("#card2").flip({ axis: 'x' }); 
 		$("#card3").flip({ axis: 'x' }); 
 		$("#card4").flip({ axis: 'x' }); 
 		$("#card5").flip({ axis: 'x' }); 
+		/*Al hacer click en la imagen ve si el src es el mismo que el del json y 
+		si lo es oculta todas las imagenes, y visualiza el gif de bien. Tambien mete 
+		en una seción la puntuación y se sale en 3 segundos. Si no te quita puntos y hace la funcion y aumenta un contador*/
 		$('img').click(function(){
 			if ($(this).attr('src') == src){
-				$('.imagen').css('display','none');
-				$('.acierto').css('display','block');
-				$('.bien').attr('src', './imagenes/bien.gif');
-				$('.bien').animate({
-					width: '700px',
-					heigth: '700px',
-					textalign: 'center'
-				});
+				$('.imagenes').css('display','none');
+				$('#bien').css('display','block');
 				sessionStorage.miPuntuaction = parseInt(sessionStorage.miPuntuaction) + puntos;//meter puntuación
 				setTimeout(() => { //Salida del juego
 					window.location.replace("../../View/vistaMapa/selectGame.html");
-				}, 1000);
+				}, 3000);
 			}
 			else {
 				puntos -= 300;
@@ -39,15 +40,19 @@ $(document).ready(function(){
 				cont++;
 			}
 		});
-
+		
+			
+		
     });
 });
+/*Estas función coje el objeto pista del json y la mete en un elemento lista con ayuda de contador anterior */
 function pistitas(pista,cont){
 	if(cont <= 2){
 		var li = $("<li>");
 		$('ul').append(li);
 		var texto_li = (pista[cont]);
+		li.attr('type','square');
 		li.append(texto_li);
-		li.css('color','white');
+
 	}	
 }
